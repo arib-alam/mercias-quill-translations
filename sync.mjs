16e7d260ab4f14e62f3b9ba4c8587fa76126cfa4
arib-alam <aribqis@gmail.com>
2025-03-27 13:50:46 -0500

    New keys and prettier formatting

--- sync.mjs
+++ sync.mjs
@@ -3,32 +3,41 @@ import { readdirSync, writeFileSync } from "fs";
 const sourceFiles = readdirSync("./sources");
 
 for (const source of sourceFiles) {
-  const { default: importedData } = await import(`./sources/${source}`, {
-    assert: { type: "json" },
-  });
-
-  const languageData = {};
-
-  for (const key in importedData) {
-    languageData[key] = { "en-GB": importedData[key] };
-  }
-
-  const localeFolders = readdirSync("./locales");
-
-  for (const locale of localeFolders) {
-    const { default: importedLocaleData } = await import(
-      `./locales/${locale}/${source}`,
-      {
+    const { default: importedData } = await import(`./sources/${source}`, {
         assert: { type: "json" },
-      }
-    );
+    });
 
-    for (const key in importedLocaleData) {
-      if (importedLocaleData[key] !== languageData[key]["en-GB"]) {
-        languageData[key][locale] = importedLocaleData[key];
-      }
+    const languageData = {};
+
+    for (const key in importedData) {
+        const formattedKey = formatKey(source, key);
+        languageData[formattedKey] = { "en-GB": importedData[key] };
     }
-  }
 
-  writeFileSync(`./json/${source}`, JSON.stringify(languageData, null, 2));
+    const localeFolders = readdirSync("./locales");
+
+    for (const locale of localeFolders) {
+        const { default: importedLocaleData } = await import(
+            `./locales/${locale}/${source}`,
+            {
+                assert: { type: "json" },
+            }
+        );
+
+        for (const key in importedLocaleData) {
+            const formattedKey = formatKey(source, key);
+            if (
+                importedLocaleData[key] !== languageData[formattedKey]["en-GB"]
+            ) {
+                languageData[formattedKey][locale] = importedLocaleData[key];
+            }
+        }
+    }
+
+    writeFileSync(`./json/${source}`, JSON.stringify(languageData, null, 2));
+}
+
+function formatKey(base, key) {
+    const baseWithoutExtension = base.replace(".json", "");
+    return key.replace(baseWithoutExtension + ".", "");
 }
